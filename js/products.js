872344d47618b336e2f53c78670e1e3c1e74b625
lucashernandezz <110567607+lucashernandezz@users.noreply.github.com>
2022-08-20 08:26:54 -0300

    Products

---
+++ js/products.js
@@ -0,0 +1,36 @@
+const link = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+document.addEventListener('DOMContentLoaded', async function() {
+    const lista = document.querySelector('.product-list-container');
+
+    const listaproductos = await getJSONData(link);
+
+    for (let products of listaproductos.data.products) {
+        lista.innerHTML += getHTML(products)
+    }
+});
+
+function getHTML(producto) {
+    return `
+    <div class="row border border-warning overflow-hidden mb-3 bg-white">
+        <div class="col-3 p-0">
+          <img class="img-fluid" src="${producto.image}" alt="">
+        </div>
+        <div style="color:rgb(255, 255, 255); background-color:rgb(31, 31, 31);"  class="col-9 d-flex flex-column justify-content-between">
+           <div class="d-flex justify-content-between">
+            <h3>${producto.name}</h3>
+                <div class="Cantvent">
+                    <p>Vendidos:  <span class="cantven">${producto.soldCount}</span></p>
+                </div>        
+            </div>  
+          <div class="d-flex justify-content-between">
+           <p>${producto.description}</p>
+            <div class="Precio">
+              <span class="moneda">${producto.currency}</span>
+              <span class="Precio">${producto.cost}</span>
+            </div>
+         </div>
+        </div>
+    </div>
+    `;
+}
