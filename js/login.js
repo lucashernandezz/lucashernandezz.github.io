59420b326a26792cf1943b4b47a73258278a645d
lucashernandezz <110567607+lucashernandezz@users.noreply.github.com>
2022-08-20 10:03:15 -0300

    Login con error

--- js/login.js
+++ js/login.js
@@ -4,5 +4,11 @@ const password = document.getElementById('contra');
 function redirect() {
     if (email.value.length >= 1 && password.value.length >= 1 ) {
         window.location.replace('/home.html')
+    } else{
+        showAlertError()
     }
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
 }
